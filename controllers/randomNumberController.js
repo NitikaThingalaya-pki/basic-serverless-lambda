a8cb33ebb91000d77e2ffc7bac09c062d91ff46c
Nitika Thingalaya <110655416+NitikaThingalaya-pki@users.noreply.github.com>
2024-08-23 10:39:13 +0530

    Update randomNumberController.js

--- controllers/randomNumberController.js
+++ controllers/randomNumberController.js
@@ -2,8 +2,7 @@ const { generateRandomNumber } = require('../src/handler');
 import * as qs from 'qs';
 
 module.exports.handle = async (event) => {
-  
     // Parse the query string from the event
-  const randomNumber = qs.parse(event.generateRandomNumber);
+  const randomNumber = qs.parse(event.randomNumber);
   return generateRandomNumber(event);
 };
